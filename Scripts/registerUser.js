9d6ed09e88bed5d766d3e5422324273373b10d0c
AadithyanRaju <aadithyan75@gmail.com>
2024-06-19 19:42:43 +0530

    Scripts

---
+++ Scripts/registerUser.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const path = require('path');
+const FabricCAServices = require('fabric-ca-client');
+const { Wallets } = require('fabric-network');
+
+'use strict';
+
+
+const ccpPath = path.resolve(__dirname, '..','..','fabric-samples','test-network','organizations','peerOrganizations','org1.example.com', 'connection-org1.json');
+
+async function main() {
+    try {
+        const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+
+        const caURL = ccp.certificateAuthorities['ca.org1.example.com'].url;
+        const ca = new FabricCAServices(caURL);
+
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+
+        const username = process.argv[2]; // Get the username from command line argument
+        if (!username) {
+            console.log('Please provide a username as a command line argument');
+            return;
+        }
+
+        const userExists = await wallet.get(username);
+        if (userExists) {
+            console.log(`An identity for the user "${username}" already exists in the wallet`);
+            return;
+        }
+
+        const adminIdentity = await wallet.get('admin');
+        if (!adminIdentity) {
+            console.log('An identity for the admin user "admin" does not exist in the wallet');
+            console.log('Run the enrollAdmin.js application before retrying');
+            return;
+        }
+
+        const provider = wallet.getProviderRegistry().getProvider(adminIdentity.type);
+        const adminUser = await provider.getUserContext(adminIdentity, 'admin');
+
+        const secret = await ca.register({
+            affiliation: 'org1.department1',
+            enrollmentID: username, // Use the username from command line argument
+            role: 'client'
+        }, adminUser);
+
+        const enrollment = await ca.enroll({
+            enrollmentID: username, // Use the username from command line argument
+            enrollmentSecret: secret
+        });
+
+        const userIdentity = {
+            credentials: {
+                certificate: enrollment.certificate,
+                privateKey: enrollment.key.toBytes(),
+            },
+            mspId: 'Org1MSP',
+            type: 'X.509',
+        };
+
+        await wallet.put(username, userIdentity);
+        console.log(`Successfully registered and enrolled admin user "${username}" and imported it into the wallet`);
+    } catch (error) {
+        console.error(`Failed to register user "${username}": ${error}`);
+        process.exit(1);
+    }
+}
+
+main();
